acbadbb9c127b700ace97529d9d674e8157d61c5
Veneno <44926802+SteveAO@users.noreply.github.com>
2021-11-07 15:32:34 +0800

    Delete IParking.java

--- IParking.java
+++
@@ -1,9 +0,0 @@
-package Git;
-
-public interface IParking {
-    public void print();
-    public boolean addCar(int carType);
-    public static IParams parse()throws Exception{
-        return null;
-    }
-}
